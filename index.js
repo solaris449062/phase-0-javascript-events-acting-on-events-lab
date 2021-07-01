0ba9c8d3328c466adc595b2f0525d11b97ae3a83
Sungoh Park <spark449062@gmail.com>
2021-07-01 02:48:38 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+let fieldStyle = getComputedStyle(document.getElementById("game"))
+let fieldWidth = parseInt(fieldStyle.width.replace("px",""), 10);
+let dodgerStyle = getComputedStyle(document.getElementById("dodger"))
+let dodgerWidth = parseInt(dodgerStyle.width.replace("px",""), 10);
+
+function moveDodgerLeft() {
+    let leftNumber = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumber, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumber = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumber, 10);
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
